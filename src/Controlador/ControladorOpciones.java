2df2b4aa95d84e607c5faab4693deb3e5c00a895
cherrerat <chudikj@live.com>
2017-06-16 13:10:22 -0400

    Controlador funcionando

--- src/Controlador/ControladorOpciones.java
+++ src/Controlador/ControladorOpciones.java
@@ -48,7 +48,7 @@ public class ControladorOpciones implements ActionListener{
         //this.vistaAgregarBus = vistaAgregarBus;
         
         this.vistaOpciones.comboBoxOpciones.addActionListener(this);
-        this.vistaOpciones.botonCerrarSesion.addActionListener(new cerrarSesion());
+        this.vistaOpciones.botonCerrarSesion.addActionListener(this);
     }
     public void InicializarOpciones(){
         
@@ -57,11 +57,44 @@ public class ControladorOpciones implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent ae) {
          o = vistaOpciones.comboBoxOpciones.getSelectedItem();
-            if(o=="Agregar Chofer"){
+            if(ae.getSource().equals(vistaOpciones.botonCerrarSesion)){
+                ControladorLogin controladorLogin = new ControladorLogin(vistaLogin, administrador);
+                vistaLogin.setVisible(true);
+                vistaOpciones.setVisible(false);
+            }else if(o=="Buscar Chofer"){
+                ControladorBuscarChofer controladorBuscarChofer = new ControladorBuscarChofer(vistaBuscarChofer, chofer);
+                vistaBuscarChofer.setVisible(true);
+                vistaOpciones.setVisible(false);
+            }else if(o == "Modificar Chofer"){
+                //
+            }else if(o == "Choferes Disponibles"){
+                ControladorChoferesDisponibles cchd = new ControladorChoferesDisponibles(vchd, chofer);
+                vchd.setVisible(true);
+                vistaOpciones.setVisible(false);
+            }else if(o=="Agregar Bus"){
+                ControladorAgregarBus controladorAgregarBus = new ControladorAgregarBus(vab, bus);
+                vab.setVisible(true);
+                vistaOpciones.setVisible(false);
+            }else if(o=="Buscar Bus"){
+                ControladorBuscarBus controladorBuscarBus = new ControladorBuscarBus(vbbus, bus);
+                vbbus.setVisible(true);
+                vistaOpciones.setVisible(false);
+            }else if(o == "Modificar Bus"){
+                //
+            }else if(o == "Buses Disponibles"){
+                ControladorBusesDisponibles controladorBusesDisponibles = new ControladorBusesDisponibles(vbd, chofer);
+                vbd.setVisible(true);
+                vistaOpciones.setVisible(false);
+            }else if(o == "Viajes Disponibles"){
+                ControladorViajesDisponibles controladorViajesDisponibles = new ControladorViajesDisponibles(vvd, viaje);
+                vvd.setVisible(true);
+                vistaOpciones.setVisible(false);
+            }else if(o=="Agregar Chofer"){
                 ControladorAgregarChofer controladorAgregarChofer = new ControladorAgregarChofer(vistaAgregarChofer, chofer);
                 vistaAgregarChofer.setVisible(true);
                 vistaOpciones.setVisible(false);
-            }
+            } 
+            
     }
     
     class seleccionOpcion implements ActionListener{
